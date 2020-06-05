17b94866beb9e08dd864d16a73d98df3b15ec189
Riley De Leacy <riley@deleacy.org>
2020-06-05 11:29:06 +1000

    - added doc for php functions

--- common_php.php
+++ common_php.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * appends zeros to the start of input $value resulting in a string of length $length
+ * @param String $value input value to be zero filled.
+ * @param Int $length desired length
+ * @return String returns $value zero filled to length $length
+ **/
 function customZeroFill($value,$length){
     $stringBuilder = $value;
     for($i=0+strlen($value);$i<$length;$i++){
@@ -6,6 +12,13 @@ function customZeroFill($value,$length){
     }
     return $stringBuilder;
 }
+/**
+ * splits a string by the nth delimiter
+ * @param String $text text to be split 
+ * @param String $delimiter delimiter to be split by
+ * @param Int $nth number of delimiters to include
+ * @return String returns the input string up intil the $nth occurance of $delimiter
+ **/
 function splitn($text,$delimiter,$nth){
     $splitstring = explode($delimiter,$text);
     $tempArray=Array();
